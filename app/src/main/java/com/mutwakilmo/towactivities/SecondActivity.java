c0f3438eb2db55c2eea9750c97d261e904c55e97
mutwakilmo <mutwakilmo@gmail.com>
2020-05-07 04:08:08 +0200

    Initial commit

---
+++ app/src/main/java/com/mutwakilmo/towactivities/SecondActivity.java
@@ -0,0 +1,35 @@
+package com.mutwakilmo.towactivities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+
+    public static final String EXTRA_REPLAY = "com.mutwakilmo.towactivities.extra.REPLAY";
+
+    private EditText mReply;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        mReply =findViewById(R.id.editText_second);
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        TextView textView = findViewById(R.id.text_message);
+        textView.setText(message);
+    }
+
+    public void returnReply(View view) {
+        String replay = mReply.getText().toString();
+        Intent replayIntent = new Intent();
+        replayIntent.putExtra(EXTRA_REPLAY,replay);
+        setResult(RESULT_OK, replayIntent);
+        finish();
+    }
+}
